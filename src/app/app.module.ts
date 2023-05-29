35b558bee5ed55afd9c136adebe27e72f19f5cd6
harry <harry.nguyen.2016@gmail.com>
2023-05-29 16:21:38 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Route, RouterModule } from '@angular/router';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { PostFormComponent } from './post-form/post-form.component';
+
+const routes : Route[] = [
+  { path:"socialPost", component : SocialPostsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialPostsComponent,
+    SinglePostComponent,
+    PostFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
